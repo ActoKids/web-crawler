20eaaebca86cea4331e4145a93f1232b2384ec5c
daonguyen81 <34518607+daonguyen81@users.noreply.github.com>
2019-02-11 19:27:07 -0800

    initial commit

--- scripts/browserCrawler/OFAScraper.py
+++ scripts/browserCrawler/OFAScraper.py
@@ -33,6 +33,7 @@ SOUP = []
 OFA = "https://outdoorsforall.org/events-news/calendar/"
 #s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
+
 def ofa_crawl(url):
     print(url)
     global QUEUE
@@ -42,103 +43,113 @@ def ofa_crawl(url):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Chrome(r'C:\Users\daong\ActoKids\web-crawler\scripts\browserCrawler\chromedriver.exe')
     driver.get(url)
-    soup = BeautifulSoup(driver.page_source, "html.parser")
     pages = 3
-    #Grabs all links on calendar
-    while pages <= 3:
+
+    # Grabs all links on calendar for 3 months from current month
+    while pages > 0:
+        soup = BeautifulSoup(driver.page_source, "html.parser")
         for row in soup.find_all("div"):
             if row.get("onclick"):
                 jsQueue.append(row.get("class")[0])
-        #driver.find_element_by_xpath("//*[@id='main_cal']/tbody/tr/td/table/tbody/tr[1]/td[3]/a").click()
-        pages += 1
+        # driver.find_element_by_xpath("//*[@id='main_cal']/tbody/tr/td/table/tbody/tr[1]/td[3]/a").click()
+        pages -= 1
         x = driver.find_elements_by_class_name(jsQueue[0])
-    count = 0
-    # Click all found elements to open page and grab the URL
-    for row in x:
-        row.click()
-        driver.switch_to.window(driver.window_handles[1])
-        if driver.current_url not in FOUND_LIST:
-            print()
-            print("Scraping for a new event...")
-            #QUEUE.append(driver.current_url)
-            FOUND_LIST.append(driver.current_url)
-            print("Links remaining - " + str(len(jsQueue)))
-            jsQueue.pop(0)
-            current_url = driver.current_url
-            current_soup = BeautifulSoup(driver.page_source, "html.parser")
-            for linebreak in current_soup.find_all('br'):
-                linebreak.extract()
-            #Calls OFAScraper module to populate a dictionary object to add to the output
-            OUTPUT[current_url] = open_link(current_soup, current_url)
-            #SOUP.append(BeautifulSoup(driver.page_source, "html.parser"))
-            driver.switch_to.window(driver.window_handles[0])
-            print("Scraping reached end of page")
+    
+        # Click all found elements to open page and grab the URL
+        for row in x:
+            row.click()
+            driver.switch_to.window(driver.window_handles[1])
+            if driver.current_url not in FOUND_LIST:
+                print()
+                print("Scraping for a new event...")
+                # QUEUE.append(driver.current_url)
+                FOUND_LIST.append(driver.current_url)
+                print("Links remaining - " + str(len(jsQueue)))
+                jsQueue.pop(0)
+                current_url = driver.current_url
+                current_soup = BeautifulSoup(driver.page_source, "html.parser")
+                for linebreak in current_soup.find_all('br'):
+                    linebreak.extract()
+                # Calls OFAScraper module to populate a dictionary object to add to the output
+                OUTPUT[current_url] = open_link(current_soup, current_url)
+                #SOUP.append(BeautifulSoup(driver.page_source, "html.parser"))
+                driver.switch_to.window(driver.window_handles[0])
+                print("Scraping reached end of page")
+            else:
+                driver.switch_to.window(driver.window_handles[0])
+            # Count is used for test purposes only.
+            #count += 1
+        driver.get(url)
+        # scrape next month from current calendar month
+        if pages == 2: 
+            driver.find_element_by_xpath("//a[img[@alt='Forward']]").click()
+        # scrape the month after next month
         else:
-            driver.switch_to.window(driver.window_handles[0])
-        #Count is used for test purposes only.
-        #count += 1
-        if count == 3:
-            break
+            driver.find_element_by_xpath("//a[img[@alt='Forward']]").click()
+            time.sleep(1)
+            driver.find_element_by_xpath("//a[img[@alt='Forward']]").click()
     driver.quit()
 
-def open_link(current_soup, current_url):
-    DATA["URL"] = current_url
-    find_title(current_soup)
-    find_date(current_soup)  
-    return DATA   
 
-def find_title(soup):
-    global DATA
+def open_link(current_soup, current_url):
+    data = {}
+    data["URL"] = current_url
+    find_title(current_soup, data)
+    find_date(current_soup, data)
+    return data
+
+
+def find_title(soup, data):
     if soup.find(class_="header-theme"):
         title = soup.find(class_="header-theme").text
-        DATA["Title"] = title
-        find_description(soup)
+        data["Title"] = title
+        find_description(soup, data)
 
-def find_description(soup):
-    global DATA
+
+def find_description(soup, data):
     desc = soup.find("span", attrs={"class": "event-desc-theme"})
-    #print(desc)
+    # print(desc)
     p_desc = ""
     loc = ""
     time = ""
-    #Look for all p elements to find description, ignore location and attempt to find time.
+    # Look for all p elements to find description, ignore location and attempt to find time.
     for row in desc.findAll("p"):
         try:
             if "Export:" not in row.text:
                 if "location" in row.text.lower():
-                   pass
+                    pass
                 else:
                     p_desc = p_desc + row.text
                 if ("pm" in row.text.lower() or "am" in row.text.lower()) and any(c.isdigit() for c in row.text):
-                    #print(row.text)
+                    # print(row.text)
                     time += row.text + " "
-                #print(row.text)
+                # print(row.text)
         except:
             pass
     if time != "":
-        DATA["Time"] = time
+        data["Time"] = time
     else:
-        DATA["Time"] = "Unknown"
+        data["Time"] = "Unknown"
     for row in desc.findAll(text=True, recursive=False):
         if(len(row) > 1):
             loc = re.sub("\r\n", "", row)
-            find_location(loc)
+            find_location(loc, data)
             break
-    DATA["Description"] = p_desc
+    data["Description"] = p_desc
 
-def find_date(soup):
-    global DATA
+
+def find_date(soup, data):
     header_re = re.compile('.*header.*')
     for row in soup.findAll(attrs={"class": header_re}):
         try:
             for val in row:
                 if parse(val):
-                    DATA["Date"] = str(parse(val))
+                    data["Date"] = str(parse(val))
                     break
         except Exception as a:
-            #print(a)
+            # print(a)
             pass
         try:
             if "pm" in str(row.lower()) or "am" in str(row.lower()):
@@ -146,16 +157,18 @@ def find_date(soup):
         except:
             pass
 
-def find_location(location):
-    global DATA
-    DATA["Location"] = location
-    #print(OUTPUT)W
+
+def find_location(location, data):
+    data["Location"] = location
+    # print(OUTPUT)W
+
 
 def create_json():
     print("creating json...")
     with open('OFA_event_data.json', 'w') as outfile:
         json.dump(OUTPUT, outfile)
     #s3.Object('mjleontest', 'browser_event_data.json').put(Body=open('browser_event_data.json', 'rb'))
+
 
 def main():
     start_time = time.time()
@@ -164,7 +177,7 @@ def main():
     while count != 5:
         try:
             print("OFA Crawl Started.")
-            #open_url(EVENT_BRITE)
+            # open_url(EVENT_BRITE)
             ofa_crawl(OFA)
             print("OFA Crawl Completed.")
             break
@@ -179,7 +192,7 @@ def main():
     elapsed_time = time.time() - start_time
     print("Crawler Ended.")
     print(elapsed_time)
-    
-if __name__ == '__main__':   
+
+
+if __name__ == '__main__':
     main()
-    
